200e9bada5b555f382457dd60afd4769700229d9
shaggy245 <shaggy245@users.noreply.github.com>
2020-07-21 21:46:29 -0500

    add check for 0 args and add basic usage

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
     "time"
     "strconv"
     "os"
+    "fmt"
 )
 
 func main() {
-    ssec, _ := strconv.Atoi(os.Args[1])
-    time.Sleep(time.Duration(ssec) * time.Second)
+    if len(os.Args) == 1 || os.Args[1] == "-h" {
+        fmt.Println("usage is:\nsleep <# of seconds to sleep>")
+    } else {
+        ssec, _ := strconv.Atoi(os.Args[1])
+        time.Sleep(time.Duration(ssec) * time.Second)
+    }
 }
